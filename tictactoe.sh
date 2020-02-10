a6fb97909bf66ad8ca82ddfc102544d094d1c627
paras <parasjitaliya@gmail.com>
2020-02-10 11:30:07 +0530

    Added:resetbord

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 echo "-----------Welcome Tic Tac Toe------------"
+rows=3
+columns=3
+resetBoard()
+{
+	for ((i=0;i<$rows;i++))
+	do
+		for ((j=0;j<$columns;j++))
+		do
+			board[$i,$j]="-"
+		done
+	done
+	echo "--Game started--"
+}
+resetBoard
